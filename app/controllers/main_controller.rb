b465079b6ae1b7693548386d1502780fc80b120e
fcontinitorres <fcontinitorres@gmail.com>
2024-03-25 17:24:15 -0300

    add flash to project;

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,8 @@
 class MainController < ApplicationController
 	def index
+		## flash -> Metodo herdado do applicationCotroller, funciona como um hash, so colocar um nome e atribuir um valor, pode ser acessado da view
+		## Vamos fazer um render partial no application contorller para ele tbm
+		# flash[:my_notification] = "Hi, I'm a flash notification!"
+
 	end
 end
